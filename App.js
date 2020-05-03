f781369b4fae9752db8ab197f70cf2407ca9fa3f
Dragon He <dragonhe36@gmail.com>
2020-05-03 01:13:37 +0100

    changes

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import {NavigationContainer} from "@react-navigation/native";
-import Home from './screens/Home.js';
-import FinancialInfo from './screens/FinancialInfo';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./screens/Home.js";
+import FinancialInfo from "./screens/FinancialInfo";
 import FinancialGoals from "./screens/FinancialGoals";
-import {createDrawerNavigator} from "@react-navigation/drawer";
+import DrawerContent from "./components/DrawerContent";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name={"Home"} component={Home}/>
-        <Drawer.Screen name={"Info"} component={FinancialInfo}/>
-        <Drawer.Screen name={"Goals"} component={FinancialGoals}/>
+      <Drawer.Navigator
+        drawerContent={({ navigation }) => (
+          <DrawerContent navigation={navigation} />
+        )}
+      >
+        <Drawer.Screen name={"Home"} component={Home} />
+        <Drawer.Screen name={"Info"} component={FinancialInfo} />
+        <Drawer.Screen name={"Goals"} component={FinancialGoals} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
--- components/DrawerContent.js
+++ components/DrawerContent.js
@@ -1,8 +1,44 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, { useState } from "react";
+import { Text, View, StatusBar, StyleSheet } from "react-native";
+import { DrawerItem } from "@react-navigation/drawer";
+import { Drawer } from "react-native-paper";
 
-export default function DrawerContent() {
+export default function DrawerContent({ navigation }) {
+  const [selected, setSelected] = useState("Home");
+  function selectItem(item) {
+    setSelected(item);
+    navigation.navigate(item);
+  }
   return (
-    <Text>Hi</Text>
+    <View style={{ marginTop: StatusBar.currentHeight }}>
+      <Drawer.Section title={"You"} style={styles.section}>
+        <DrawerItem
+          label={"Overview"}
+          onPress={() => selectItem("Home")}
+          focused={selected === "Home"}
+        />
+      </Drawer.Section>
+      <Drawer.Section title={"Finances"} style={styles.section}>
+        <DrawerItem
+          label={"Info"}
+          onPress={() => selectItem("Info")}
+          focused={selected === "Info"}
+        />
+        <DrawerItem
+          label={"Goals"}
+          onPress={() => selectItem("Goals")}
+          focused={selected === "Goals"}
+        />
+      </Drawer.Section>
+      <Drawer.Section title={"Recommendations"}>
+        <DrawerItem label={"Credit Cards"} onPress={() => {}} focused={false} />
+      </Drawer.Section>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  section: {
+    paddingBottom: 20,
+  },
+});
